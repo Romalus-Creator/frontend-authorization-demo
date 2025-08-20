a7b0c1166dfe1dccbe71338c44b100a81fde3aa6
Romalus-Creator <86391110+Romalus-Creator@users.noreply.github.com>
2025-08-19 21:43:45 -0500

    Feat: Protect Routes on front end and setup Registration per example instructions.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,56 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import * as auth from "../utils/auth";
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
 import Register from "./Register";
+import ProtectedRoute from "./ProtectedRoute";
 import "./styles/App.css";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleRegistration = ({
+    username,
+    email,
+    password,
+    confirmPassword,
+  }) => {
+    if (password === confirmPassword) {
+      auth
+        .register(username, password, email)
+        .then(() => {
+          // Navigate user to login page.
+          navigate("/login");
+        })
+        .catch(console.error);
+    }
+  };
+
   return (
     <Routes>
-      <Route path="/ducks" element={<Ducks />} />
-      <Route path="/my-profile" element={<MyProfile />} />
+      {/*ducks*/}
+      <Route
+        path="/ducks"
+        element={
+          <ProtectedRoute isLoggedIn={isLoggedIn}>
+            <Ducks />
+          </ProtectedRoute>
+        }
+      />
+      {/*my-profile*/}
+      <Route
+        path="/my-profile"
+        element={
+          <ProtectedRoute isLoggedIn={isLoggedIn}>
+            <MyProfile />
+          </ProtectedRoute>
+        }
+      />
+      {/*login*/}
       <Route
         path="/login"
         element={
@@ -18,12 +59,23 @@ function App() {
           </div>
         }
       />
+      {/*register*/}
       <Route
         path="/register"
         element={
           <div className="registerContainer">
-            <Register />
+            <Register handleRegistration={handleRegistration} />
           </div>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          isLoggedIn ? (
+            <Navigate to="/ducks" replace />
+          ) : (
+            <Navigate to="/login" replace />
+          )
         }
       />
     </Routes>
